e7622b3045d7cf2f1548b745b33e199ddad6541b
thymelous <tim.labushev@gmail.com>
2018-06-11 15:45:53 +0300

    vertex renaming

--- site/src/hw4.js
+++ site/src/hw4.js
@@ -2,10 +2,18 @@ import { h } from 'preact';
 
 export default function(matrix) {
   const [ hamiltonianCycle, rfLog ] = robertsFlores(matrix);
+  const reorderedMatrix = reorderVertices(matrix, hamiltonianCycle);
+
   return (
     <div>
       <h2>Нахождение гамильтонова цикла</h2>
       <p class="multiline">{rfLog.join('\n')}</p>
+      <h2>Матрица смежности с перенумерованными вершинами</h2>
+      <table>
+        {reorderedMatrix.map((row, rowi) => (<tr>{
+          row.map((cell, coli) => (<td>{coli > rowi ? (<strong><u>{cell}</u></strong>) : cell}</td>))
+        }</tr>))}
+      </table>
     </div>
   );
 }
@@ -24,6 +32,15 @@ function findIndexes(array, val) { return array.reduce((a, el, i) => (el == val)
 
 function setWith(set, el) { return new Set(set).add(el) }
 
+// https://stackoverflow.com/a/14986106/1726690
+function splitOverlapping(array, chunkSize) {
+  const res = [];
+  for (let i = 0; i < array.length - (chunkSize - 1); i++) {
+    res.push(array.slice(i, i + chunkSize));
+  }
+  return res;
+}
+
 function robertsFlores(matrix) {
   const vertices = matrix.length;
   const log = [];
@@ -31,7 +48,8 @@ function robertsFlores(matrix) {
   const adjacent = Array(vertices).fill().map((_, i) => findIndexes(matrix[i], 1));
   log.push(`Включаем в $S$ вершину $x_1$. $S = \\{x_1\\}$`);
   
-  return robertsFloresStep(matrix, adjacent, 0, 0, new Set([0]), new DeepSet(), log);
+  const [ pathSet, _ ] = robertsFloresStep(matrix, adjacent, 0, 0, new Set([0]), new DeepSet(), log);
+  return [Array.from(pathSet), log];
 }
 
 function robertsFloresStep(matrix, adjacent, vPrev, v, path, pathsTaken, log) {
@@ -65,4 +83,19 @@ function robertsFloresStep(matrix, adjacent, vPrev, v, path, pathsTaken, log) {
   const vNext = lastInSet(path);
   log.push(`У ${v + 1} больше нет возможных вершин, удалим ее. Перейдем к ${vNext + 1}. ${pathStr()}`);
   return robertsFloresStep(matrix, adjacent, v, vNext, path, pathsTaken, log);
+}
+
+function reorderVertices(matrix, hamiltonianCycle) {
+  const cycleEdges = splitOverlapping(hamiltonianCycle, 2);
+  const renamedVertices = new Map(hamiltonianCycle.map((v, i) => [i, v]));
+
+  const newMatrix = [];
+  for (let a = 0; a < matrix.length; a++) {
+    newMatrix[a] = []
+    for (let b = 0; b < matrix.length; b++) {
+      newMatrix[a][b] = matrix[renamedVertices.get(a)][renamedVertices.get(b)];
+    }
+  }
+
+  return newMatrix;
 }
